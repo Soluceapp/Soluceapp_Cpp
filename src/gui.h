1636b46817da8a52a96ba4a41520a8cd7bb5ccba
Soluceapp <139369754+Soluceapp@users.noreply.github.com>
2023-08-13 19:10:39 +0300

    initial commit
    initial commit

---
+++ src/gui.h
@@ -0,0 +1,50 @@
+#ifndef SRC_GUI_H_
+#define SRC_GUI_H_
+#pragma once
+#include<d3d9.h>
+
+namespace gui
+{
+// constant window size
+constexpr int WIDTH = 500;
+constexpr int HEIGHT = 300;
+
+inline bool exit = true;
+
+//winapi window wars
+inline HWND window = nullptr;
+inline WNDCLASSEXA windowClass = {};
+
+// points for window movement
+inline POINTS position = {};
+
+// direct x state vars
+inline PDIRECT3D9 d3d = nullptr;
+inline LPDIRECT3DDEVICE9 device = nullptr;
+inline D3DPRESENT_PARAMETERS presentParameters = {};
+
+//handle window creation & destruction
+void CreateHWindow(
+		const char*windowName,
+		const char*className) noexcept;
+void DestroyHWindow() noexcept;
+
+//handle device creation & destruction
+bool CreateDevice() noexcept;
+void ResetDevice() noexcept;
+void DestroyDevice() noexcept;
+
+// handle ImGui creation & destruction
+void CreateImGui() noexcept;
+void DestroyImGui() noexcept;
+
+void BeginRender() noexcept;
+void EndRender() noexcept;
+void Render() noexcept;
+
+}
+
+
+
+
+#endif /* SRC_GUI_H_ */
